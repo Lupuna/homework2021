503c4ec3ad93d9ce1f537bf27894d2b99de039e9
Lupuna <90504544+Lupuna@users.noreply.github.com>
2021-09-14 12:51:30 +0700

    Add files via upload

--- lecture01.py
+++ lecture01.py
@@ -4,6 +4,14 @@ Spyder Editor
 
 This is a temporary script file.
 """
-a = float(input())
-print (1.8 * a + 32)
-input()
+import math
+a = float (input ("Ведите длину стороны а:" ))
+b = float (input ('Ведите длину стороны в:' ))
+cos = float (input ('Введите значение угла между сторонами:' ))
+if cos >1 :
+    print (math.sqrt(a**2 + b**2 - 2 * math.cos(math.degrees(cos)) * a * b))
+elif cos >=0 and cos < 1:
+    print (math.sqrt(a**2 + b**2 - 2 * cos * a * b))
+else:
+    print ('Я не умею это считать.')
+input ()
