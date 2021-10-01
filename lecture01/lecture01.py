d0311826f19e26d68acccb3bc733d4f81f4d47f5
Lupuna <90504544+Lupuna@users.noreply.github.com>
2021-10-01 21:32:42 +0700

    Update lecture01.py

--- lecture01/lecture01.py
+++ lecture01/lecture01.py
@@ -5,8 +5,10 @@ Spyder Editor
 This is a temporary script file.
 """
 import math
+
 a = float (input ("Ведите длину стороны а:" ))
 b = float (input ('Ведите длину стороны в:' ))
+
 cos = float (input ('Введите значение угла между сторонами:' ))
 if cos >1 :
     print (math.sqrt(a**2 + b**2 - 2 * math.cos(math.degrees(cos)) * a * b))
