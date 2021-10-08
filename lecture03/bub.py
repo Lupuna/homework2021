3d0287444773eb7e5a80c77b9f10ebc18c10ec57
Lupuna <90504544+Lupuna@users.noreply.github.com>
2021-10-08 19:20:48 +0700

    Add files via upload

--- lecture03/bub.py
+++ lecture03/bub.py
@@ -1,10 +1,20 @@
-from math import gcd
 try:
-    
-     a = int(input())
-     b = int(input())
+     x = int(input())
+     y = int(input())
+     
+     def gcd (a, b):
+          while a != 0 and b != 0:
+               if a > b:
+                    a = a % b
+               else:
+                    b = b % a
+          return a+b
+     
+          
+     print (gcd(x, y))
+          
+     
 
-     print (gcd(a,b))
 except:
     print ('Как дела?') 
 
